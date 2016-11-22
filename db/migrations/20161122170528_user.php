28bfa16d412eac650874131c7905fd5f63b47e18
wykay <wykay@iMac-de-wykay.local>
2016-11-22 11:11:31 -0600

    first commit with the migration DB

---
+++ db/migrations/20161122170528_user.php
@@ -0,0 +1,42 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class User extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $user = $this->table('user');
+        $user
+            ->addColumn('email', 'string', ['limit' => 100, 'null' => false])
+            ->addColumn('first_name', 'string', ['limit' => 50, 'null' => false])
+            ->addColumn('last_name', 'string', ['limit' => 50, 'null' => false])
+            ->addColumn('password', 'string', ['limit' => 255, 'null' => false])
+            ->addColumn('created_at', 'timestamp', ['null' => false, 'default' => 'CURRENT_TIMESTAMP'])
+            ->addColumn('updated_at', 'timestamp', ['null' => false, 'default' => 'CURRENT_TIMESTAMP'])
+        ;
+
+        $user->create();
+    }
+}
